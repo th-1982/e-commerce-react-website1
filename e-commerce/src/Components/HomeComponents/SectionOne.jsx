ca80ed7ffcdb1bf0bf878a77c32202c1a5d93bc7
Theresa Wolff <theresawolff@gmail.com>
2024-05-17 11:36:07 +0200

    Add the react icons and fire emoji to the sectionOne components

--- e-commerce/src/Components/HomeComponents/SectionOne.jsx
+++ e-commerce/src/Components/HomeComponents/SectionOne.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './SectionOne.css'
 import image1 from '../images/hero-right.webp'
+import { FiSearch } from "react-icons/fi";
 
 const SectionOne = () => {
   return (
-    <div>
-    <div>
-      <p>In this season, find the best</p>
+    <div className='homeSectionOne'>
+    <div className='firstSection'>
+      <p>In this season, find the best 🔥</p>
       <h2>Exclusive collection for everyone</h2>
-      <button>Explore Now</button>
+      <button>Explore now <FiSearch /> </button>
     </div>
-    <div>
+    <div className='secondSection'>
         <img src={image1} alt="" />
     </div>
     </div>
