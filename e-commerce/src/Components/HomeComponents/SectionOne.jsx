55ca12a9b6975d85e0dfed308bd44b842c7b4bdf
Theresa Wolff <theresawolff@gmail.com>
2024-05-18 21:50:07 +0200

    Import the two images into SectionOne component

--- e-commerce/src/Components/HomeComponents/SectionOne.jsx
+++ e-commerce/src/Components/HomeComponents/SectionOne.jsx
@@ -1,18 +1,50 @@
 import React from 'react'
 import './SectionOne.css'
 import image1 from '../images/hero-right.webp'
+import image2 from '../images/hero-right-2.webp'
+import image3 from '../images/hero-right-3.webp'
 import { FiSearch } from "react-icons/fi";
 
 const SectionOne = () => {
   return (
+
+    <div>
+
     <div className='homeSectionOne'>
     <div className='firstSection'>
       <p>In this season, find the best 🔥</p>
       <h2>Exclusive collection for everyone</h2>
       <button>Explore now <FiSearch /> </button>
     </div>
+
     <div className='secondSection'>
         <img src={image1} alt="" />
+    </div>
+    </div>
+
+   
+    <div className='homeSectionOne'>
+    <div className='firstSection'>
+      <p>In this season, find the best 🔥</p>
+      <h2>Exclusive collection for everyone</h2>
+      <button>Explore now <FiSearch /> </button>
+    </div>
+
+    <div className='secondSection'>
+        <img src={image2} alt="" />
+    </div>
+    </div>
+
+    <div className='homeSectionOne'>
+    <div className='firstSection'>
+      <p>In this season, find the best 🔥</p>
+      <h2>Exclusive collection for everyone</h2>
+      <button>Explore now <FiSearch /> </button>
+    </div>
+
+    <div className='secondSection'>
+        <img src={image3} alt="" />
+    </div>
     </div>
     </div>
   )
